a26566fee726efa5b58598f39677b118d3104ac6
BURTONI_INFO <BURTONI_INFO@CFPI-R006PC03>
2014-05-07 08:50:37 +0200

    ajout de la selection de la colonne pour l'ajout du nouvel item

--- script/mainscript.js
+++ script/mainscript.js
@@ -1,7 +1,7 @@
 /* Main script Ownlink Project
  * Autors: OLIVEIRA Stéphane & BURTON Ian
  * Date: 30.4.2014
-*/
+ */
 $(document).ready(function() {
     Ownlink.init();
     
@@ -10,8 +10,9 @@ $(document).ready(function() {
 var Ownlink = { 
     init: function() {
         $("#add").click(function(){
-            Ownlink.ajout()
-            });
+            $(".column").css("background-color","#99ff99");
+            $(".column").click(function() {Ownlink.ajout($(this))});
+        });
         $( ".column" ).sortable({
             connectWith: ".column",
             placeholder: "ui-state-highlight"
@@ -20,8 +21,11 @@ var Ownlink = {
 
     },
     
-    ajout: function() {
-        $("#divLeft").append("<div class='item'></div>");  
+    ajout: function($column) {            
+        $column.append("<div class='item'></div>");
+        $(".column").css("background-color","#ffffff");
+        $column = NULL;
+        
     }
 }; 
 
